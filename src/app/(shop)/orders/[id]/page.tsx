bd59a5daa038c56ca2c4552470aa72db5dad1d87
Gustavoand39 <111763968+Gustavoand39@users.noreply.github.com>
2024-06-13 13:39:11 -0600

    feat: Update order page layout and styles, and fix status color bug

--- src/app/(shop)/orders/[id]/page.tsx
+++ src/app/(shop)/orders/[id]/page.tsx
@@ -32,8 +32,8 @@ export default function OrderPage({ params }: Props) {
               className={clsx(
                 "flex items-center rounded-lg py-2 px-3.5 text-sm font-bold text-white mb-5",
                 {
-                  "bg-red-500": true,
-                  "bg-green-700": false,
+                  "bg-red-500": false,
+                  "bg-green-700": true,
                 }
               )}
             >
