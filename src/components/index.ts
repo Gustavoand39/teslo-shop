0e1352359e96699c9b4d4c5600e80541fb95028a
Gustavoand39 <111763968+Gustavoand39@users.noreply.github.com>
2024-06-13 13:54:13 -0600

    feat: Add Footer component for shop layout

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,7 +2,7 @@ export * from "./ui/top-menu/TopMenu";
 export * from "./ui/not-found/PageNotFound";
 export * from "./ui/title/Title";
 export * from "./ui/sidebar/Sidebar";
-
+export * from "./ui/footer/Footer";
 
 export * from "./product/slideshow/ProductSlideShow";
 export * from "./product/slideshow/ProductMobileSlideShow";
---
+++ src/components/ui/footer/Footer.tsx
@@ -0,0 +1,25 @@
+import { titleFont } from "@/config/font";
+import Link from "next/link";
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <footer className="flex w-full justify-center text-sm mb-10">
+      <Link href="/home">
+        <span className={`${titleFont.className} antialiased font-bold`}>
+          Teslo {" "}
+        </span>
+        <span>| Shop</span>
+        <span>&copy; {new Date().getFullYear()}</span>
+      </Link>
+
+      <Link href="/" className="mx-3">
+        Privacidad y términos
+      </Link>
+
+      <Link href="/" className="mx-3">
+        Ubicanos
+      </Link>
+    </footer>
+  );
+};
