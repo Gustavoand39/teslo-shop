6c8675affb32a5ead0a9809a9cba68b88d47d416
Gustavoand39 <111763968+Gustavoand39@users.noreply.github.com>
2024-06-12 20:06:04 -0600

    chore: Update Swiper component with autoplay feature and add thumbnail slideshow

--- src/components/product/slideshow/ProductSlideShow.tsx
+++ src/components/product/slideshow/ProductSlideShow.tsx
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 import { Swiper as SwiperObject } from "swiper";
 import { SwiperSlide, Swiper } from "swiper/react";
-import { FreeMode, Navigation, Thumbs } from "swiper/modules";
+import { Autoplay, FreeMode, Navigation, Thumbs } from "swiper/modules";
 
 import "swiper/css";
 import "swiper/css/free-mode";
@@ -32,9 +32,32 @@ export const ProductSlideShow = ({ images, title, className }: Props) => {
         }
         spaceBetween={10}
         navigation={true}
+        autoplay={{ delay: 3000 }}
         thumbs={{ swiper: thumbsSwiper }}
-        modules={[FreeMode, Navigation, Thumbs]}
+        modules={[FreeMode, Navigation, Thumbs, Autoplay]}
         className="mySwiper2"
+      >
+        {images.map((image) => (
+          <SwiperSlide key={image}>
+            <Image
+              width={1024}
+              height={800}
+              src={`/products/${image}`}
+              alt={title}
+              className="rounded-lg object-fill"
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+
+      <Swiper
+        onSwiper={setThumbsSwiper}
+        spaceBetween={10}
+        slidesPerView={4}
+        freeMode={true}
+        watchSlidesProgress={true}
+        modules={[FreeMode, Navigation, Thumbs]}
+        className="mySwiper"
       >
         {images.map((image) => (
           <SwiperSlide key={image}>
