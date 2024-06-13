5470a5de44b019169c02d59080b4726398c35be3
Gustavoand39 <111763968+Gustavoand39@users.noreply.github.com>
2024-06-12 19:56:49 -0600

    feat: Add Swiper component for product image slideshow

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,6 +3,8 @@ export * from "./ui/not-found/PageNotFound";
 export * from "./ui/title/Title";
 export * from "./ui/sidebar/Sidebar";
 
+
+export * from "./product/slideshow/ProductSlideShow";
 export * from "./product/size-selector/SizeSelector";
 export * from "./product/quantity-selector/QuantitySelector";
 
